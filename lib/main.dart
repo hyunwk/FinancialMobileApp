f5f5ec94d7a123cd9f99a01acf80e627ce434926
hyunwk <ab8454@naver.com>
2023-01-21 23:14:43 +0900

    feat: row header

--- lib/main.dart
+++ lib/main.dart
@@ -12,36 +12,39 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            backgroundColor: Colors.black,
-            body: Column(
-              children: [
-                SizedBox(
-                  height: 80,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Text(
-                          "Hey, Caleb",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 38,
-                              fontWeight: FontWeight.w500),
-                        ),
-                        Text(
-                          "Welcome back",
-                          style: TextStyle(
-                            color: Colors.white.withOpacity(0.8),
+            backgroundColor: Color(0xFF181818),
+            body: Padding(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 80,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            "Hey, Caleb",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 28,
+                                fontWeight: FontWeight.w500),
                           ),
-                        )
-                      ],
-                    )
-                  ],
-                )
-              ],
+                          Text(
+                            "Welcome back",
+                            style: TextStyle(
+                              color: Colors.white.withOpacity(0.8),
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ),
             )));
   }
 }
